cf1beafb96585510cdb46932a69690c7b4a6182f
Sandro Wirth <s.wirth303@gmail.com>
2018-01-09 18:48:05 +0100

    Purity explanation

---
+++ src/main/java/PurityExplanation.java
@@ -0,0 +1,71 @@
+import java.awt.*;
+
+public class PurityExplanation {
+
+    private int field;
+    private static int staticField;
+
+    /**
+     *  Purity: Stateless
+     */
+    public int stateless(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     *  Purity: Stateful   <br>
+     *
+     *  Return value depends on the following:
+     *  <ul>
+     *  <li>    Argument: a (int)     </li>
+     *  </ul>
+     */
+    public int stateful(int a) {
+        return a + this.field;
+    }
+
+    /**
+     * Purity: FieldModifier
+     */
+    public void fieldModifier(int a) {
+        this.field = a;
+    }
+
+    /**
+     * Purity: StaticModifier    <br>
+     *
+     * Modifies the following static fields:
+     * <ul>
+     * <li>    {@link PurityExplanation#staticField}     </li>
+     * </ul>
+     */
+    public void staticModifier(int a) {
+        staticField = a;
+    }
+
+    /**
+     * Purity: ArgumentModifier     <br>
+     *
+     * Modifies the following arguments:
+     * <ul>
+     * <li>    rectangle     </li>
+     * </ul>
+     */
+    public void argumentModifier(Rectangle rectangle) {
+        rectangle.height = 100;
+    }
+
+    /**
+     * Purity: Native    <br>
+     *
+     * The method calls native code:
+     * <ul>
+     * <li>    {@link PurityExplanation#nativeMethod()}    </li>
+     * </ul>
+     */
+    public void nativeCall() {
+        nativeMethod();
+    }
+
+    private native void nativeMethod();
+}
